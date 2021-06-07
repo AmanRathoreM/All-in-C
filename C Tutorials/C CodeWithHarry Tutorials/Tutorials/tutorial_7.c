3a823c5f9fea96f3de3eee2c3543af1b2f727ce0
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-06-07 10:21:23 +0530

    Add files via upload

---
+++ C Tutorials/C CodeWithHarry Tutorials/Tutorials/tutorial_7.c
@@ -0,0 +1,123 @@
+// Date 06-06-2021
+
+#include <stdio.h>
+
+int main()
+{
+    // float a = 54.36445455;
+    // printf("Value of Float A is %i", a);
+
+    int a, b;
+    a = 2;
+    b = 3;
+
+    printf("%d + %d = %d\n", a, b, a + b);
+    printf("%d - %d = %d\n", a, b, a - b);
+    printf("%d * %d = %d\n", a, b, a * b);
+    printf("%d / %d = %d\n", a, b, a / b);
+    printf("There are many more operators but they are already tought in C++ tutorials");
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    //* There are many more operators but they are already tought in C++ tutorials
+    // printf("\n%d", 10 << 3);
+    // printf("\n%d", 10 << 3);
+    // printf("\n%d", 2 != 2 ?: 3);
+    return 0;
+}
