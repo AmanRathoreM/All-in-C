47a4988ee2f8e327bb2d85c7b94678c79c930cd6
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-06-08 09:41:32 +0530

    Add files via upload

---
+++ C Tutorials/C CodeWithHarry Tutorials/Tutorials/tutorial_9_2_4.c
@@ -0,0 +1,11 @@
+// Date 06-06-2021
+
+#include <stdio.h>
+
+int main()
+{
+    float tutorial_9_2_4 = 99.999;
+    printf("To print float use %%f \'%f\'\n", tutorial_9_2_4);
+    printf("To print float use %%e \'%e\'\n", tutorial_9_2_4);
+    return 0;
+}
