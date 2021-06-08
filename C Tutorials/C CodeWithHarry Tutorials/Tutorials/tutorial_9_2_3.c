b30a73c0ac60c14eda9e8f7566478505d4d66b76
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-06-08 09:39:10 +0530

    Add files via upload

---
+++ C Tutorials/C CodeWithHarry Tutorials/Tutorials/tutorial_9_2_3.c
@@ -0,0 +1,11 @@
+// Date 06-06-2021
+
+#include <stdio.h>
+
+int main()
+{
+    int tutorial_9_2_3_1 = 2015, tutorial_9_2_3_2 = 2020;
+    printf("To print int use %%d \'%d\'\n", tutorial_9_2_3_1);
+    printf("To print int use %%i \'%i\'\n", tutorial_9_2_3_2);
+    return 0;
+}
