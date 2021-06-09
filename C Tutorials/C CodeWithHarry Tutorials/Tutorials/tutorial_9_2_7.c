88f352c627efee65d813ccaa8dfc942795fa0844
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-06-09 11:35:08 +0530

    Add files via upload

---
+++ C Tutorials/C CodeWithHarry Tutorials/Tutorials/tutorial_9_2_7.c
@@ -0,0 +1,12 @@
+// Date 06-06-2021
+
+#include <stdio.h>
+
+int main()
+{
+    char tutorial_9_2_7;
+    printf("To take input as char use %%c and in front of second argunment add \'&\'\n");
+    scanf("%c", &tutorial_9_2_7);
+    printf("You entered \'%c\'\n", tutorial_9_2_7);
+    return 0;
+}
