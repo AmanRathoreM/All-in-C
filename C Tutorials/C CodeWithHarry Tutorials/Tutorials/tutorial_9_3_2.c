dfd200e8ce47305db28c3144c461c1ca28844289
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-06-09 11:36:19 +0530

    Add files via upload

---
+++ C Tutorials/C CodeWithHarry Tutorials/Tutorials/tutorial_9_3_2.c
@@ -0,0 +1,13 @@
+// Date 06-06-2021
+
+#include <stdio.h>
+
+int main()
+{
+    const float pi = 3.141592653;
+    printf("Constant Pi is! %f", pi);
+
+    //* pi = 3.14; //This will give an error because pi is a constant
+
+    return 0;
+}
