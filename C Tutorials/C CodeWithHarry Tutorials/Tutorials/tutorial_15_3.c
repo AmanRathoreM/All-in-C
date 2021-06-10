a9c3ba46e6380f0134c37f6137ad8e8995fb83df
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-06-10 09:42:08 +0530

    Add files via upload

---
+++ C Tutorials/C CodeWithHarry Tutorials/Tutorials/tutorial_15_3.c
@@ -0,0 +1,16 @@
+// Date 07-06-2021
+
+#include <stdio.h>
+
+int main()
+{
+    int index_1 = 1, index_2 = 0;
+    printf("Initial value of index_1 is %d!\n\n\n", index_2);
+
+    for (; index_2 = 99, index_1 <= 10; index_1++)
+    {
+        printf("Value of index_1 is %d!\n", index_2);
+        printf("Hello Aman %d!\n", index_1);
+    }
+    return 0;
+}
