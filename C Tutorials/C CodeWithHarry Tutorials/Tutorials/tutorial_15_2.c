b18c91ec12c50217d2b494a38a28f1beaa0c417a
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-06-10 09:41:48 +0530

    Add files via upload

---
+++ C Tutorials/C CodeWithHarry Tutorials/Tutorials/tutorial_15_2.c
@@ -0,0 +1,14 @@
+// Date 07-06-2021
+
+#include <stdio.h>
+
+int main()
+{
+    int index = 1;
+
+    for (; index <= 100; index++)
+    {
+        printf("Hello Aman %d!\n", index);
+    }
+    return 0;
+}
