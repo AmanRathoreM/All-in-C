a576572da67246c33ef6d18b2f48e3aeb09f6f25
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-06-11 11:04:51 +0530

    Add files via upload

---
+++ C Tutorials/C CodeWithHarry Tutorials/Tutorials/tutorial_18_2.c
@@ -0,0 +1,12 @@
+// Date 08-06-2021
+
+#include <stdio.h>
+
+int main()
+{
+    float pi = (float)22 / 7;
+    float PI = 22 / 7;
+    printf("The value of PI is %f\n", PI);
+    printf("The value of pi is %f\n", pi);
+    return 0;
+}
