081a3b659b3bd379898c9a7c63b24c2990869f12
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-06-12 10:55:00 +0530

    Add files via upload

---
+++ C Tutorials/C CodeWithHarry Tutorials/Tutorials/tutorial_19_1.c
@@ -0,0 +1,15 @@
+// Date 08-06-2021
+
+#include <stdio.h>
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    printf("Sum function return value is %d", sum(7, 3));
+
+    return 0;
+}
