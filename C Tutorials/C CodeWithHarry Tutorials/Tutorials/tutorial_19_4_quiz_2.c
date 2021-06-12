b42224fdf31e713af19ab97db6bdba711154f43a
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-06-12 10:56:02 +0530

    Add files via upload

---
+++ C Tutorials/C CodeWithHarry Tutorials/Tutorials/tutorial_19_4_quiz_2.c
@@ -0,0 +1,119 @@
+// Date 08-06-2021
+
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+//* Make a function which takes no arguments an returns nothing
+
+#include <stdio.h>
+
+void print();
+
+int main()
+{
+    printf("By running print function we get\n");
+    print();
+
+    return 0;
+}
+
+void print(void)
+{
+    printf("This is written inside print function");
+}
