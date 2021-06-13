a3bf79d3abfc090ac53be1b5bf22a818e772d10d
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-06-13 09:56:20 +0530

    Add files via upload

---
+++ C Tutorials/C CodeWithHarry Tutorials/Tutorials/tutorial_26_2.c
@@ -0,0 +1,21 @@
+// Date 09-06-2021
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    int *pointer_of_a = &a;
+    printf("Pointers are printed by using \'%%p\' which is a dedicated Format Specifier for Pointer\n");
+    printf("The value of \'a\' is %d\n", a);
+    printf("The value of \'&a\' is %p\n", &a);
+    printf("The value of \'pointer_of_a\' is %p\n", pointer_of_a);
+    printf("The value of \'*pointer_of_a\' is %d\n", *pointer_of_a);
+    printf("The value of \'&pointer_of_a\' is %p\n", &pointer_of_a);
+    printf("The size of \'pointer_of_a\' is %d\n", sizeof(pointer_of_a));
+    // printf("The size of \'pointer_of_a\' is %d\n", sizeof(int *));
+    // printf("The value of Pointer of A is %d\n", &pointer_of_a);
+    // printf("");
+
+    return 0;
+}
