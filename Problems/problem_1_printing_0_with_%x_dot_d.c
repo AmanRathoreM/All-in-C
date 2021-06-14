7ad51e320c6d9ebfa1fa44bdaba55986210ab844
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-06-14 10:42:19 +0530

    Add files via upload

---
+++ Problems/problem_1_printing_0_with_%x_dot_d.c
@@ -0,0 +1,25 @@
+// Date 12-06-2021
+
+//* In this problem I can't able to print 0 with %x.d i.e. %1.d, %2.d, %3.d............ etc.
+//* In this problem I can't able to print 0 with %x.d i.e. %1.d, %2.d, %3.d............ etc.
+//* In this problem I can't able to print 0 with %x.d i.e. %1.d, %2.d, %3.d............ etc.
+//* In this problem I can't able to print 0 with %x.d i.e. %1.d, %2.d, %3.d............ etc.
+//* In this problem I can't able to print 0 with %x.d i.e. %1.d, %2.d, %3.d............ etc.
+//* In this problem I can't able to print 0 with %x.d i.e. %1.d, %2.d, %3.d............ etc.
+//* In this problem I can't able to print 0 with %x.d i.e. %1.d, %2.d, %3.d............ etc.
+//* In this problem I can't able to print 0 with %x.d i.e. %1.d, %2.d, %3.d............ etc.
+//* In this problem I can't able to print 0 with %x.d i.e. %1.d, %2.d, %3.d............ etc.
+
+#include <stdio.h>
+
+int main()
+{
+    int num_1 = 0;
+    int num_2 = 10000;
+    int num_3 = 999;
+
+    printf("Value of \'num_1\' is = %7.d\n", num_1);
+    printf("Value of \'num_2\' is = %7.d\n", num_2);
+    printf("Value of \'num_3\' is = %7.d\n", num_3);
+    return 0;
+}
