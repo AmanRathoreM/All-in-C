d4cd31e3c51da894be02ddac9891616519b24588
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-06-14 14:55:40 +0530

    Add files via upload

---
+++ Problems/problem_1_printing_0_with_%x_dot_d__solution__.c
@@ -0,0 +1,25 @@
+// Date 12-06-2021
+
+//* This solution of problem in "problem_1_printing_0_with_%x_dot_d.c"
+//* This solution of problem in "problem_1_printing_0_with_%x_dot_d.c"
+//* This solution of problem in "problem_1_printing_0_with_%x_dot_d.c"
+//* This solution of problem in "problem_1_printing_0_with_%x_dot_d.c"
+//* This solution of problem in "problem_1_printing_0_with_%x_dot_d.c"
+//* This solution of problem in "problem_1_printing_0_with_%x_dot_d.c"
+//* This solution of problem in "problem_1_printing_0_with_%x_dot_d.c"
+//* This solution of problem in "problem_1_printing_0_with_%x_dot_d.c"
+//* This solution of problem in "problem_1_printing_0_with_%x_dot_d.c"
+
+#include <stdio.h>
+
+int main()
+{
+    int num_1 = 0;
+    int num_2 = 10000;
+    int num_3 = 999;
+
+    printf("Value of \'num_1\' is = %7d\n", num_1);
+    printf("Value of \'num_2\' is = %7d\n", num_2);
+    printf("Value of \'num_3\' is = %7d\n", num_3);
+    return 0;
+}
