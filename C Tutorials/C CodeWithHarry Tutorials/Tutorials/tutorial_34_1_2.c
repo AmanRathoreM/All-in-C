61f4f20d6838094178a8a1728c100e70593baa3f
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-06-14 10:05:01 +0530

    Add files via upload

---
+++ C Tutorials/C CodeWithHarry Tutorials/Tutorials/tutorial_34_1_2.c
@@ -0,0 +1,125 @@
+// Date 10-06-2021
+
+#include <stdio.h>
+
+void print_string(char string[])
+{
+    int i = 0;
+    while (string[i] != '\0')
+    {
+        printf("%c", string[i++]);
+    }
+    printf("\nThis string (array) has length of %d\n", i);
+}
+
+int main()
+{
+    //* The below two lines are a good practice to declare a string
+    char str[] = "Aman";
+    print_string(str);
+    //* The above two lines are a good practice to declare a string
+
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+    //* Note: char str[] = "Aman" and char str[] = {'A', 'm', 'a', 'n', '\0'} are same for compiler
+
+    // printf("");
+    return 0;
+}
