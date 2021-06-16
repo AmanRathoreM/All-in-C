d0a545aae202aa7e5a74038ea97471f6df415e89
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-06-16 13:20:37 +0530

    Add files via upload

---
+++ C Tutorials/C CodeWithHarry Tutorials/Tutorials/tutorial_34_2_quiz_5.c
@@ -0,0 +1,16 @@
+// Date 10-06-2021
+
+//* Why the following lines of code will give an Error
+//! char str[5];
+//! str = "HELLO";
+
+#include <stdio.h>
+
+int main()
+{
+    // char str[5];
+    // str = "H";
+    // printf("Hello Aman!");
+
+    return 0;
+}
