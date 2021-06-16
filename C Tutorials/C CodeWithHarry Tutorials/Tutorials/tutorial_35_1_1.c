cea032068ececffc8f9d5964b1d73f93d4793b0e
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-06-16 13:21:08 +0530

    Add files via upload

---
+++ C Tutorials/C CodeWithHarry Tutorials/Tutorials/tutorial_35_1_1.c
@@ -0,0 +1,14 @@
+// Date 10-06-2021
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str_1[] = "Aman";
+    char str_2[] = "Aman";
+
+    puts(strcat(str_1, str_2));
+
+    return 0;
+}
