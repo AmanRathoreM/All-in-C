65c2092e760022dfe3d46925c65e8b0049d02812
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-06-16 13:20:12 +0530

    Add files via upload

---
+++ C Tutorials/C CodeWithHarry Tutorials/Tutorials/tutorial_34_1_5.c
@@ -0,0 +1,17 @@
+// Date 10-06-2021
+
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+
+    printf("Enter a string with a length of 100 program will take input from you by using an studio function \'puts()\'\n");
+    gets(str);
+
+    printf("You entered the following words this is printed by using an studio function \'puts()\'\n");
+    puts(str);
+
+    // printf("");
+    return 0;
+}
